48310f8d11eaf9793af1483b4de20d3871ae1bd7
Martin Rader <m1rader@edu.aau.at>
2024-06-05 15:28:55 +0200

    Change from Mario: gpt query now allows for term exclusions with '!' term

--- app/core/query_transform/mongodb/query_parts/qpt_gpt.py
+++ app/core/query_transform/mongodb/query_parts/qpt_gpt.py
@@ -1,5 +1,3 @@
-import unicodedata
-
 from core.query_transform.base.query_parts.query_part_transformer_base import QueryPartTransformerBase
 from core import settings
 from core.utils.solr_util import SolrUtil
@@ -16,18 +14,29 @@ class QPTGPT(QueryPartTransformerBase):
         return ["message"]
 
     def transform(self, result_object, query_dict, debug_info, *args, **kwargs):
-        search_term = query_dict.get("gpt")
+        search_term = query_dict.get("gpt").strip()
         words = search_term.split(" ")
 
+        # Identify inclusion and exclusion terms
+        include_words = [word for word in words if not word.startswith("!")]
+        exclude_words = [word[1:] for word in words if word.startswith("!")]
+
         # Construct exact match phrase query with a boost
-        exact_match_query = f'description:"{search_term}"^2.0'
+        exact_match_query = f'description:"{" ".join(include_words)}"^2.0'
 
         # Construct fuzzy match queries
-        fuzzy_terms = [f"description:{word}~0.7" for word in words]
+        fuzzy_terms = [f"description:{word}~0.7" for word in include_words]
         fuzzy_match_query = " OR ".join(fuzzy_terms)
 
-        # Combine exact match and fuzzy match queries
-        search_query = f"{exact_match_query} OR ({fuzzy_match_query})"
+        # Construct exclude queries
+        exclude_queries = [f"-description:{word}" for word in exclude_words]
+        exclude_query = " AND ".join(exclude_queries)
+
+        # Combine exact match, fuzzy match, and exclude queries
+        if exclude_query:
+            search_query = f"({exact_match_query} OR ({fuzzy_match_query})) AND {exclude_query}"
+        else:
+            search_query = f"({exact_match_query} OR ({fuzzy_match_query}))"
 
         solr_url = settings.SOLR_URL
         message = kwargs.get("message")
@@ -42,3 +51,4 @@ class QPTGPT(QueryPartTransformerBase):
             result_object["$and"] = [mongo_query]
         else:
             result_object["$and"].append(mongo_query)
+
