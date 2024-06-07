ae01cc05799254a5dbfe768102ebd92463fcd5e6
Martin Rader <m1rader@edu.aau.at>
2024-06-07 20:12:14 +0200

    add unix timestamp to log

--- app/core/utils/log_util.py
+++ app/core/utils/log_util.py
@@ -1,5 +1,5 @@
 import json
-
+import time  # Importiere die Time-Bibliothek
 
 class LogUtil:
     logfile = 'queries.log'
@@ -8,10 +8,11 @@ class LogUtil:
     def write_to_queries_log(message):
         LogUtil.write_dict_to_file(LogUtil.logfile, message)
 
-
     @staticmethod
     def write_dict_to_file(file_path, message):
+        # Holen des aktuellen Unix Timestamp
+        timestamp = int(time.time())
         # Open file in append mode, which creates the file if it doesn't exist
         with open(file_path, 'a') as file:
-            # Write the dictionary to a new line
-            file.write(message + '\n')
+            # Füge den Timestamp vor dem Nachrichteninhalt ein
+            file.write(f"{timestamp}: {message}\n")
